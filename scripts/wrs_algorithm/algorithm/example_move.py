cd6981c3b4be8555d95b428d9ea406ed3f4aa155
Yuhei MATSUMURA <yh-matsumura@sec.co.jp>
2021-09-09 17:47:57 +0900

    [change]サンプルプログラムのimportを整理
    unicode_literalをimportと、moveit_commanderがエラーになるため
    追加しなかった。

--- scripts/wrs_algorithm/algorithm/example_move.py
+++ scripts/wrs_algorithm/algorithm/example_move.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function, division, absolute_import
 import rospy
 import sys
-import math
 
 from wrs_algorithm.util import omni_base, whole_body, gripper, simulator, mathematics
 
